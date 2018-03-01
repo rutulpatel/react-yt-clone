3ed01d518283f0e4ead6e306a79cb732f9fdcf03
Rutul Patel <rutulpatel@Rutuls-MacBook-Pro.local>
2018-02-28 19:57:13 -0600

    Add code for all components of youtube react app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = "AIzaSyC5Inhpfl-7LhFL8NXNnY7mvWewZQyQB-w";
 
-const App = () => {
+
+
+class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = { videos: [], selectedVideo: null };
+
+    this.searchVideos("red grapes");
+  }
+
+  searchVideos(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0] 
+      });
+    });
+  }
+ 
+  render() {
+    const searchVideos = _.debounce((term) => {this.searchVideos(term)}, 300);
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={searchVideos} />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
     </div>
   );
+}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
